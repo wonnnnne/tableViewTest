6865eea44223152d7177050cb0c3ea01a4ae00ad
TaewonLee <twlee9212@gmail.com>
2019-11-28 20:28:55 +0900

    Custom tableView cell

---
+++ test/CustomTableCell.swift
@@ -0,0 +1,32 @@
+//
+//  CustomTableCell.swift
+//  test
+//
+//  Created by Taewon Lee on 11/28/19.
+//  Copyright © 2019 Taewon Lee. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- test/ViewController.swift
+++ test/ViewController.swift
@@ -12,6 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+        
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,17 +42,28 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
     
     // 셀이 몇개 인지 설정해주는 함수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return restaurantNames.count
     }
     
     
     // 셀을 어떻게 구성할것인지에 대한 함수
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-        let tv = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
+        let tv = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath) as! CustomTableCell
         
-        tv.textLabel?.text = "test"
+        tv.titleLabel.text = restaurantNames[indexPath.row]
+        tv.locationLabel.text = restaurantLocations[indexPath.row]
+        tv.typeLabel.text = restaurantTypes[indexPath.row]
+        tv.imgView.image = UIImage(named: restaurantImages[indexPath.row])
+        
+        
+        
         return tv
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath)
     }
     
     
